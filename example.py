ffeb186174c99b40e38e8be15a722ea87c8bab68
Madda <nobody@nowhere.local>
2015-03-18 10:19:52 +0000

    Add headers

--- example.py
+++ example.py
@@ -3,16 +3,19 @@ import urllib
 import urllib2
 
 
-def call_local_url(path, post=False, post_data={}):
+def call_local_url(path, post=False, post_data={}, headers={}):
     if post:
-        response = urllib2.urlopen(
+        request = urllib2.Request(
             'http://127.0.0.1/{path}'.format(path=path),
             urllib.urlencode(post_data),
+            headers=headers,
             )
     else:
-        response = urllib2.urlopen(
-            'http://127.0.0.1/{path}'.format(path=path)
+        request = urllib2.Request(
+            'http://127.0.0.1/{path}'.format(path=path),
+            headers=headers,
             )
+    response = urllib2.urlopen(request)
     data = response.read()
     return data
 
@@ -21,3 +24,8 @@ if __name__ == '__main__':
     print(call_local_url(path=''))
     print('===========')
     print(call_local_url(path='', post=True, post_data={'test': 1}))
+    print('===========')
+    print(call_local_url(path='', headers={'User-Agent': 'woogle'}))
+    print('===========')
+    print(call_local_url(path='', post=True, post_data={'test': 1},
+                         headers={'User-Agent': 'wooglepost'}))
