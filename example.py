e14b90dc7cd582a8f0866d94e8cf9368e555927b
Madda <nobody@nowhere.local>
2015-03-18 10:16:20 +0000

    Add post functionality

--- example.py
+++ example.py
@@ -1,12 +1,23 @@
 #! /usr/bin/env python
+import urllib
 import urllib2
 
 
-def get_local_url(path):
-    response = urllib2.urlopen('http://127.0.0.1/{path}'.format(path=path))
+def call_local_url(path, post=False, post_data={}):
+    if post:
+        response = urllib2.urlopen(
+            'http://127.0.0.1/{path}'.format(path=path),
+            urllib.urlencode(post_data),
+            )
+    else:
+        response = urllib2.urlopen(
+            'http://127.0.0.1/{path}'.format(path=path)
+            )
     data = response.read()
     return data
 
 
 if __name__ == '__main__':
-    print(get_local_url(path=''))
+    print(call_local_url(path=''))
+    print('===========')
+    print(call_local_url(path='', post=True, post_data={'test': 1}))
